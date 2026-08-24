144e5135684bded975bfe405c0374701a34f362d
Đào Đức Anh <daoducanh1000@gmail.com>
2026-08-25 00:36:08 +0700

    Update EnergyOrb.cs

--- Assets/Script/Collectibles/EnergyOrb.cs
+++ Assets/Script/Collectibles/EnergyOrb.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class EnergyOrb : MonoBehaviour
 {
+    [SerializeField] private int energyValue = 5;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
         if (GameManager.Instance == null) return;
 
         Debug.Log("EnergyOrb Taken"); 
-        GameManager.Instance.IncreaseCurrentEnergy(1);
+        GameManager.Instance.IncreaseCurrentEnergy(energyValue);
             
         Destroy(gameObject);
     }
